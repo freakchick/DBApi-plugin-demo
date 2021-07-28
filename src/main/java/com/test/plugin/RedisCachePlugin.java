907fef6d47b788f0a4bf57938fe3eae4b4575efb
jiangqiang <1032355091@qq.com>
2021-07-28 20:53:23 +0800

    init

---
+++ src/main/java/com/test/plugin/RedisCachePlugin.java
@@ -0,0 +1,77 @@
+package com.test.plugin;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.jq.dbapi.common.ApiConfig;
+import com.jq.dbapi.plugin.CachePlugin;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+
+import java.util.List;
+import java.util.Map;
+
+public class RedisCachePlugin implements CachePlugin {
+    @Override
+    public void set(ApiConfig apiConfig, Map<String, Object> map, Object data) {
+
+        Jedis jedis = null;
+        try {
+            JedisPool pool = JedisUtil.getPool();
+            jedis = pool.getResource();
+            String key = "api-" + apiConfig.getId();
+            String hashKey = "";
+            for (Object o : map.values()) {
+                hashKey += o.toString() + "-";
+            }
+            jedis.hset(key, hashKey, JSON.toJSONString(data));
+        } catch (Exception e) {
+            System.out.println(e);
+        } finally {
+            if (jedis != null) {
+                jedis.close();
+            }
+        }
+    }
+
+    @Override
+    public void clean(ApiConfig apiConfig) {
+        Jedis jedis = null;
+        try {
+            JedisPool pool = JedisUtil.getPool();
+            jedis = pool.getResource();
+            String key = "api-" + apiConfig.getId();
+            jedis.del(key);
+        } catch (Exception e) {
+            System.out.println(e);
+        } finally {
+            if (jedis != null) {
+                jedis.close();
+            }
+        }
+
+    }
+
+    @Override
+    public Object get(ApiConfig apiConfig, Map<String, Object> map) {
+        Jedis jedis = null;
+        try {
+            JedisPool pool = JedisUtil.getPool();
+            jedis = pool.getResource();
+            String key = "api-" + apiConfig.getId();
+            String hashKey = "";
+            for (Object o : map.values()) {
+                hashKey += o.toString() + "-";
+            }
+            String hget = jedis.hget(key, hashKey);
+            List<JSONObject> list = JSON.parseArray(hget, JSONObject.class);
+            return list;
+        } catch (Exception e) {
+            System.out.println(e);
+            return null;
+        } finally {
+            if (jedis != null) {
+                jedis.close();
+            }
+        }
+    }
+}
