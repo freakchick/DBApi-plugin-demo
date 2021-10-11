84a06118375097673e35b8e1121adcca77841f61
Administrator <jiangqiang110@126.com>
2021-10-11 19:08:20 +0800

    注释

--- src/main/java/com/gitee/freakchicken/demo/plugin/EncryptTransformerPlugin.java
+++ src/main/java/com/gitee/freakchicken/demo/plugin/EncryptTransformerPlugin.java
@@ -2,7 +2,6 @@ package com.gitee.freakchicken.demo.plugin;
 
 import com.alibaba.fastjson.JSONObject;
 import com.gitee.freakchicken.dbapi.common.ApiConfig;
-import com.gitee.freakchicken.dbapi.plugin.PluginConf;
 import com.gitee.freakchicken.dbapi.plugin.TransformPlugin;
 import org.apache.commons.codec.digest.DigestUtils;
 import org.apache.commons.lang3.StringUtils;
@@ -13,6 +12,13 @@ import java.util.List;
  * md5字段加密插件
  */
 public class EncryptTransformerPlugin extends TransformPlugin {
+
+    /**
+     * sql查询结果数据转换
+     * @param data
+     * @param config
+     * @return
+     */
     @Override
     public Object transform(List<JSONObject> data, ApiConfig config) {
         String params = config.getTransformPluginParams();
