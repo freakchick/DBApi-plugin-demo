b9c72c36d08925b524dd3a7943ec575c22ddb557
jiangqiang <jiangqiang110@126.com>
2022-02-20 10:54:01 +0800

    3.0.0版本插件

--- src/main/java/com/gitee/freakchicken/demo/plugin/EncryptTransformerPlugin.java
+++ src/main/java/com/gitee/freakchicken/demo/plugin/EncryptTransformerPlugin.java
@@ -1,7 +1,6 @@
 package com.gitee.freakchicken.demo.plugin;
 
 import com.alibaba.fastjson.JSONObject;
-import com.gitee.freakchicken.dbapi.common.ApiConfig;
 import com.gitee.freakchicken.dbapi.plugin.TransformPlugin;
 import org.apache.commons.codec.digest.DigestUtils;
 import org.apache.commons.lang3.StringUtils;
@@ -12,6 +11,11 @@ import java.util.List;
  * md5字段加密插件
  */
 public class EncryptTransformerPlugin extends TransformPlugin {
+
+    @Override
+    public void init() {
+        super.logger.info("EncryptTransformerPlugin init ...");
+    }
 
     /**
      * sql查询结果数据转换
